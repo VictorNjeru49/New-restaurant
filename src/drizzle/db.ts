0fe8e77ce82771114da3df21c672df08346d73f4
VictorNjeru49 <VictorNjeru49@gmail.com>
2024-06-13 11:13:13 +0300

    dist commit

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -3,7 +3,7 @@ import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from "./schema"
 
-export const client = neon(process.env.DATABASE_URL as string) //as string
+export const client = neon(process.env.Database_URL!) //as string
 
 const db = drizzle(client, { schema, logger: true });
 
